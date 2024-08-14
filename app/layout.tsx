116b7c4479a3325d0df62510a57b6ac177f0ff7b
Ankith G <ankithg03@gmail.com>
2024-08-15 03:54:04 +0530

    Inital commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 // This is the root layout component for your Next.js app.
 // Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
+import type { Metadata } from "next";
 import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import './globals.css'
@@ -16,12 +17,18 @@ const fontBody = Inter({
   variable: '--font-body',
 })
 
+const inter = Inter({ subsets: ["latin"] });
 
-interface LayoutProps {
+export const metadata: Metadata = {
+  title: "Gruha Pravesha",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
   children: React.ReactNode;
-}
-
-export const Layout: React.FC<LayoutProps> = ({ children }) => {
+}>) {
   return (
     <html lang="en">
       <body 
@@ -36,4 +43,3 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
     </html>
   )
 }
-export default Layout;
